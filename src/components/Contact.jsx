39ecdd78ce06af43e05b16414d070d2acb3732de
ahmedhussein74 <ahmedhusseinofficial22@gmail.com>
2023-08-09 15:31:28 +0300

    links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "../style/Contact.css";
+import facebook from "../images/facebook.png";
+import linkedin from "../images/linkedin.png";
+import whatsapp from "../images/whatsapp.png";
+import github from "../images/github.png";
+import gmail from "../images/gmail.png";
 
 const Contact = () => {
   return (
@@ -12,19 +17,19 @@ const Contact = () => {
       <h2 className="text-white text-lg">Web Developer</h2>
       <div className="icons flex pt-2">
         <a href="https://www.facebook.com/AhmedHusein22/">
-          <img src="/src/images/facebook.png" />
+          <img src={facebook} />
         </a>
         <a href="https://www.linkedin.com/in/ahmed-hussein-23b95b1b8">
-          <img src="/src/images/linkedin.png" />
+          <img src={linkedin} />
         </a>
         <a href="https://wa.me/qr/6VYZYWSEUQMEC1">
-          <img src="/src/images/whatsapp.png" />
+          <img src={whatsapp} />
         </a>
         <a href="https://github.com/ahmedhussein74">
-          <img src="/src/images/github.png" />
+          <img src={github} />
         </a>
         <a href="mailto:ahmedhusseinofficial22@gmail.com">
-          <img src="/src/images/gmail.png" />
+          <img src={gmail} />
         </a>
       </div>
     </section>
